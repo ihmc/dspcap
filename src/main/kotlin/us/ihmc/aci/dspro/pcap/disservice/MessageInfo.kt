78c8411b1e6e8b970ae7a1709bbdee5cf8e6eafe
gbenincasa <gbenincasa@ihmc.us>
2017-11-02 18:42:08 -0400

    Added workaround for pkt.io bug in Buffer class.

--- src/main/kotlin/us/ihmc/aci/dspro/pcap/disservice/MessageInfo.kt
+++ src/main/kotlin/us/ihmc/aci/dspro/pcap/disservice/MessageInfo.kt
@@ -15,8 +15,10 @@ class MessageInfo(private var buf: Buffer) {
     val objectId: String
     val instanceId: String
     val referredObjectId: String
-    val annotationMetadata: Buffer
+    val annotationMetadata: ByteArray
     val tag: Int
+    val clientId: Int
+    val clientType: Short
     val mimeType: String
     val totalLength: Long
     val fragmentOffset: Long
@@ -33,16 +35,25 @@ class MessageInfo(private var buf: Buffer) {
         group = readString(buf, buf.readUnsignedShort())
         publisher = readString(buf, buf.readUnsignedShort())
         sequenceId = buf.readUnsignedInt()
+            buf.readInt()  // TODO: workaround because readUnsignedInt() does not increment index
         chunkId = buf.readUnsignedByte()
         objectId = readString(buf, buf.readUnsignedShort())
         instanceId = readString(buf, buf.readUnsignedShort())
         referredObjectId = readString(buf, buf.readUnsignedShort())
-        annotationMetadata = buf.readBytes(buf.readUnsignedInt().toInt())
+        var annotationMetadataLen = buf.readUnsignedInt()
+            buf.readInt()  // TODO: workaround because readUnsignedInt() does not increment index
+        annotationMetadata = if (annotationMetadataLen > 0) buf.readBytes(annotationMetadataLen.toInt()).array
+                    else ByteArray(0)
         tag = buf.readUnsignedShort()
+        clientId = buf.readUnsignedShort()
+        clientType = buf.readUnsignedByte()
         mimeType = readString(buf, buf.readUnsignedShort())
         totalLength = buf.readUnsignedInt()
+            buf.readInt()  // TODO: workaround because readUnsignedInt() does not increment index
         fragmentOffset = buf.readUnsignedInt()
+            buf.readInt()  // TODO: workaround because readUnsignedInt() does not increment index
         fragmentLength = buf.readUnsignedInt()
+            buf.readInt()  // TODO: workaround because readUnsignedInt() does not increment index
         historyWindow = buf.readUnsignedShort()
         priority = buf.readUnsignedByte()
         buf.readBytes(8);   // Expiration
@@ -55,6 +66,7 @@ class MessageInfo(private var buf: Buffer) {
             referredObject = buf.readBytes(buf.readUnsignedShort()).array
             buf.readUnsignedByte()
             buf.readUnsignedInt()
+                buf.readInt()  // TODO: workaround because readUnsignedInt() does not increment index
             totalNumberOfChunks = 0
         }
     }
