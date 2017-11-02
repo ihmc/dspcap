eb88fb584693a7e7d9acdc67b1adfb8b056591d2
gbenincasa <gbenincasa@ihmc.us>
2017-11-02 18:36:05 -0400

    Removed suspension.

--- bin/parse.bat
+++ bin/parse.bat
@@ -1,4 +1,4 @@
 @echo off
 
-set dbg="-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=8989"
+set dbg="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8989"
 java %dbg% -jar ..\build\libs\dspcap-all-1.0.jar ..\test\resources\pcap\*.pcap
