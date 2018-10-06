5274fc1e7ff920fa382306aad0df9613875e02fb
giacobenin <giacomo.benincasa@gmail.com>
2018-10-06 00:22:26 -0400

    Added support for sqlite parsing

--- src/main/kotlin/us/ihmc/aci/dspro/pcap/PcapReader.kt
+++ src/main/kotlin/us/ihmc/aci/dspro/pcap/PcapReader.kt
@@ -1,10 +1,22 @@
 package us.ihmc.aci.dspro.pcap
 
 import io.pkts.Pcap
+import io.pkts.buffer.Buffer
+import io.pkts.buffer.InputStreamBuffer
 import io.pkts.packet.UDPPacket
+import kotliquery.*
+import java.io.ByteArrayInputStream
+import java.io.File
+import java.sql.DriverManager
 
-class PcapReader(private val filename: String) {
-    fun parse() {
+abstract class DSProDataReader(protected val filename: String) {
+    
+    abstract fun parse()
+}
+
+class PcapReader(filename: String) : DSProDataReader(filename) {
+    
+    override fun parse() {
         Pcap.openStream(filename).loop { packet ->
             if (packet.hasProtocol(io.pkts.protocol.Protocol.UDP)) {
                 var udp = packet.getPacket(io.pkts.protocol.Protocol.UDP) as UDPPacket
@@ -20,12 +32,47 @@ class PcapReader(private val filename: String) {
     }
 }
 
+class SqliteReader(filename: String)  : DSProDataReader(filename){
+    private val conn = DriverManager.getConnection("jdbc:sqlite:$filename")
+    private val session = Session(Connection(conn), false)
+
+    data class Message(val sourceTimestamp: Long, val data: Buffer)
+
+    override fun parse() {
+        val timestamp = "arrivalTimeStamp"
+        val data = "data"
+        val query = """
+            SELECT arrivalTimeStamp, $data
+            FROM DisServiceDataCache
+            ORDER BY $timestamp
+        """
+        val results = queryOf(query).map {Message(
+                it.long(1),
+                InputStreamBuffer(ByteArrayInputStream(it.bytes(2)))
+            )
+        }.asList
+        session.run(results).forEach {
+            try {
+                val msg = DisServiceMessage(it.data).getMessage(Protocol.DSPro)
+                println(msg)
+            }
+            catch (ex: Exception) {
+                println(ex.message)
+            }
+        }
+    }
+}
+
 fun main(args: Array<String>) {
     if (args.isEmpty()) {
         println("Usage: java -jar <capture.pcap>")
     }
     for (filename in args) {
-        println("Reading, $filename");
-        PcapReader(filename).parse()
+        val parser= when(File(filename).extension.toLowerCase()) {
+            "sqlite" -> SqliteReader(filename)
+            else -> PcapReader(filename)
+        }
+        println("Reading, $filename")
+        parser.parse()
     }
 }
